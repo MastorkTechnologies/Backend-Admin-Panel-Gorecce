64a664afbb7d7572468c4a95919e1e3ac81b62bd
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-08-29 21:26:16 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,5 @@
 module.exports = {
     JWT_SECRET: "sj2klsxbeddbsajdjsdbd",
-    SENDGRID_API:
-      "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",
+    SENDGRID_API:"",
     EMAIL_FROM: "satyamchaurasia2011@gmail.com",
   };
